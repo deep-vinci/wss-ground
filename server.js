6927f02bb84a51d871615d7c89d638f34a0d7ffd
deep-vinci <deepakworkpc@gmail.com>
2026-02-20 00:38:45 +0530

    chore: styling fixes

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const wss = new WebSocket.Server({ server });
 wss.on("connection", function connection(ws) {
     ws.on("message", function incoming(data) {
         wss.clients.forEach(function each(client) {
+            console.log(client.isPaused);
             const msgString = data.toString();
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(msgString);
             }
         });
+
+        console.log("");
     });
 });
 
